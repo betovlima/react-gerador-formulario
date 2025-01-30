f0e52b153fb4f5de9dc8de5065cc151ae54faaef
roberto_lima <roberto_lima@sicredi.com.br>
2025-01-30 15:51:01 -0300

    Refatoração

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,123 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import FormControl from "@mui/material/FormControl";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
-import Button from "@mui/material/Button";
-const styles = {
-  question: {
-    padding: "20px",
-    margin: "10px",
-    border: "1px solid #ccc",
-    borderRadius: "4px",
-  },
-  button: {
-    padding: "8px 10px",
-    fontSize: "14px",
-    backgroundColor: "#34a853",
-    color: "#FFF",
-    border: "none",
-    minWidth: "100px",
-    borderRadius: "4px",
-    cursor: "pointer",
-    marginRight: "10px",
-    "&:hover": {
-      backgroundColor: "#45a049",
-    },
-  },
-  input: {
-    width: "100%",
-    padding: "8px",
-    marginBottom: "10px",
-  },
-};
 
 const Question = ({
   question,
   index,
   questions,
   setQuestions,
-  moveQuestionUp,
-  moveQuestionDown,
-  deleteQuestion,
-  handleNestedOpen,
 }) => {
-  const [editing, setEditing] = useState(false);
-  const [open, setOpen] = React.useState(false);
-  const [answeringIndex, setAnsweringIndex] = React.useState(null);
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [movedIndex, setMovedIndex] = useState(null);
-
-  const addAnswer = (index) => {
-    handleClickOpen(index);
-  };
-
-  const handleClickOpen = (index) => {
-    setOpen(true);
-    setAnsweringIndex(index);
-  };
-
-  const handleEditQuestion = () => {
-    setEditing(!editing);
-  };
-
   return (
-    <div
-      style={
-        index === movedIndex
-          ? {
-              ...styles.question,
-              border: "4px solid #4CAF50",
-              animation: "blink 1.5s ease",
-            }
-          : styles.question
-      }
-    >
-      <div
-        style={{
-          display: "flex",
-          marginBottom: "10px",
-          justifyContent: "flex-end",
-        }}
-      >
-        <Button onClick={() => moveQuestionUp(index)} style={styles.button}>
-          Mover para cima
-        </Button>
-        <Button onClick={() => moveQuestionDown(index)} style={styles.button}>
-          Mover para baixo
-        </Button>
-        <Button onClick={handleEditQuestion} style={styles.button}>
-          {editing ? "Salvar" : "Editar"}
-        </Button>
-        <Button onClick={() => deleteQuestion(index)} style={styles.button}>
-          Excluir
-        </Button>
-      </div>
-      {editing ? (
-        <input
-          type="text"
-          value={question.text}
-          onChange={(e) => {
-            const newQuestions = [...questions];
-            newQuestions[index].text = e.target.value;
-            setQuestions(newQuestions);
-          }}
-          style={styles.input}
-        />
-      ) : (
-        <h3>
-          {index + 1}. {question.text}
-        </h3>
-      )}
+    <h3>
+      {index + 1}. {question.text}
+      <br />
       {question.answers.length > 0 && (
         <FormControl component="fieldset">
           <RadioGroup
-            aria-label="answers"
-            name={`answers-${index}`}
-            value={question.selectedAnswers[0] || ""}
+            aria-label="respostas"
+            name={`respostas-${index}`}
+            value={question.selectedAnswers}
             onChange={(e) => {
               const newQuestions = [...questions];
-              newQuestions[index].selectedAnswers = [e.target.value];
+              newQuestions[index].selectedAnswers = e.target.value;
               setQuestions(newQuestions);
             }}
           >
@@ -132,39 +37,7 @@ const Question = ({
           </RadioGroup>
         </FormControl>
       )}
-      <div>
-        <Button onClick={() => addAnswer(index)} style={styles.button}>
-          Adicionar Resposta
-        </Button>
-        <Button onClick={() => handleNestedOpen(index)} style={styles.button}>
-          Aninhar Pergunta
-        </Button>
-      </div>
-
-      {/* Renderiza as sub-perguntas, se existirem */}
-      {question.nestedQuestions && question.nestedQuestions.length > 0 && (
-        <div style={{ marginLeft: "20px" }}>
-          {question.nestedQuestions.map((nestedQuestion, nestedIndex) => (
-            <Question
-              key={nestedIndex}
-              question={nestedQuestion}
-              index={nestedIndex}
-              questions={question.nestedQuestions} // Passa as sub-perguntas como `questions`
-              setQuestions={(newNestedQuestions) => {
-                // Função para atualizar as sub-perguntas
-                const newQuestions = [...questions];
-                newQuestions[index].nestedQuestions = newNestedQuestions;
-                setQuestions(newQuestions);
-              }}
-              moveQuestionUp={moveQuestionUp}
-              moveQuestionDown={moveQuestionDown}
-              deleteQuestion={deleteQuestion}
-              handleNestedOpen={handleNestedOpen}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    </h3>
   );
 };
 
--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Question from "./Question";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -8,86 +7,32 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
+import DialogContentText from "@mui/material/DialogContentText";
+import Question from "./Question";
 
 const QuestionList = ({ questions, setQuestions }) => {
-  const [currentQuestion, setCurrentQuestion] = useState("");
-  const [, setAnswers] = useState({});
-  const [movedIndex, setMovedIndex] = useState(null);
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [open, setOpen] = React.useState(false);
-  const [selectedAnswer, setSelectedAnswer] = React.useState("");
-  const [answeringIndex, setAnsweringIndex] = React.useState(null);
-
   const [nestedOpen, setNestedOpen] = React.useState(false);
   const [nestedQuestion, setNestedQuestion] = React.useState("");
+  const [movedIndex, setMovedIndex] = useState(null);
   const [nestingIndex, setNestingIndex] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [answeringIndex, setAnsweringIndex] = React.useState(null);
+  const [selectedAnswer, setSelectedAnswer] = React.useState("");
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [, setAnswers] = useState({});
 
   const handleNestedOpen = (index) => {
     setNestedOpen(true);
     setNestingIndex(index);
   };
 
-  const handleNestedClose = () => {
-    setNestedOpen(false);
-    setNestedQuestion(""); // Limpa o texto da sub-pergunta
-  };
-
-  const addNestedQuestion = () => {
-    if (nestedQuestion.trim() === "") return;
-
-    const newQuestions = [...questions];
-    if (!newQuestions[nestingIndex].nestedQuestions) {
-      newQuestions[nestingIndex].nestedQuestions = [];
-    }
-    newQuestions[nestingIndex].nestedQuestions.push({
-      text: nestedQuestion,
-      answers: [],
-      selectedAnswers: [],
-    });
-    setQuestions(newQuestions);
-    setNestedOpen(false);
-    setNestedQuestion("");
-  };
-
-  const handleCheckboxChange = (event) => {
-    const value = event.target.value;
-    if (event.target.checked) {
-      setSelectedAnswer([...selectedAnswer, value]);
-    } else {
-      setSelectedAnswer(selectedAnswer.filter((item) => item !== value));
-    }
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleAnswer = () => {
-    const newQuestions = [...questions];
-    newQuestions[answeringIndex].answers = selectedAnswer;
-    setQuestions(newQuestions);
-    setOpen(false);
-    setSelectedAnswer([]);
-  };
-
   const addAnswer = (index) => {
     handleClickOpen(index);
   };
+
   const handleClickOpen = (index) => {
     setOpen(true);
     setAnsweringIndex(index);
-  };
-
-  const addQuestion = () => {
-    if (currentQuestion.trim() === "") return;
-    setQuestions([
-      ...questions,
-      {
-        text: currentQuestion,
-        answers: [],
-      },
-    ]);
-    setCurrentQuestion("");
   };
 
   const moveQuestionUp = (index) => {
@@ -147,20 +92,116 @@ const QuestionList = ({ questions, setQuestions }) => {
     );
   };
 
+  const handleAnswer = () => {
+    const newQuestions = [...questions];
+    newQuestions[answeringIndex].answers = selectedAnswer;
+    setQuestions(newQuestions);
+    setOpen(false);
+    setSelectedAnswer([]);
+  };
+
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    if (event.target.checked) {
+      setSelectedAnswer([...selectedAnswer, value]);
+    } else {
+      setSelectedAnswer(selectedAnswer.filter((item) => item !== value));
+    }
+  };
+
+  const handleNestedClose = () => {
+    setNestedOpen(false);
+    setNestedQuestion("");
+  };
+
+  const addNestedQuestion = () => {
+    if (nestedQuestion.trim() === "") return;
+
+    const newQuestions = [...questions];
+    if (!newQuestions[nestingIndex].nestedQuestions) {
+      newQuestions[nestingIndex].nestedQuestions = [];
+    }
+    newQuestions[nestingIndex].nestedQuestions.push({
+      text: nestedQuestion,
+      answers: [],
+      selectedAnswers: [],
+    });
+    setQuestions(newQuestions);
+    setNestedOpen(false);
+    setNestedQuestion("");
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div style={styles.form}>
+    <div style={styles.container}>
       {questions.map((question, index) => (
-        <Question
+        <div
           key={index}
-          question={question}
-          index={index}
-          questions={questions}
-          setQuestions={setQuestions}
-          moveQuestionUp={moveQuestionUp}
-          moveQuestionDown={moveQuestionDown}
-          deleteQuestion={deleteQuestion}
-          handleNestedOpen={editQuestion}
-        />
+          style={
+            index === movedIndex
+              ? {
+                  ...styles.question,
+                  border: "4px solid #4CAF50",
+                  animation: "blink 1.5s ease",
+                }
+              : styles.question
+          }
+        >
+          <div
+            style={{
+              display: "flex",
+              marginBottom: "10px",
+              justifyContent: "flex-end",
+            }}
+          >
+            <button
+              onClick={() => moveQuestionUp(index)}
+              style={styles.buttonWithMargin}
+            >
+              Mover para cima
+            </button>
+            <button
+              onClick={() => moveQuestionDown(index)}
+              style={styles.buttonWithMargin}
+            >
+              Mover para baixo
+            </button>
+            <button
+              onClick={() => editQuestion(index)}
+              style={styles.buttonWithMargin}
+            >
+              {editingIndex === index ? "Salvar" : "Editar"}
+            </button>
+            <button
+              onClick={() => deleteQuestion(index)}
+              style={styles.buttonWithMargin}
+            >
+              Excluir
+            </button>
+          </div>
+          {editingIndex === index ? (
+            <input
+              type="text"
+              value={questions[index].text}
+              onChange={(e) => {
+                const newQuestions = [...questions];
+                newQuestions[index].text = e.target.value;
+                setQuestions(newQuestions);
+              }}
+              style={styles.input}
+            />
+          ) : (
+            <Question
+              index={index}
+              question={question}
+              setQuestions={setQuestions}
+            />
+          )}
+          {renderQuestion(question, index)}
+        </div>
       ))}
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Selecione a resposta</DialogTitle>
@@ -191,13 +232,71 @@ const QuestionList = ({ questions, setQuestions }) => {
           <Button onClick={handleAnswer}>Adicionar</Button>
         </DialogActions>
       </Dialog>
+
+      <Dialog open={nestedOpen} onClose={handleNestedClose}>
+        <DialogTitle>Aninhar Pergunta</DialogTitle>
+        <DialogContent
+          style={{ width: "600px", maxWidth: "80%", padding: "20px" }}
+        >
+          <DialogContentText>Digite a sub-pergunta:</DialogContentText>
+          <textarea
+            autoFocus
+            margin="dense"
+            id="name"
+            type="text"
+            fullWidth
+            width="450px"
+            heigth="400px"
+            variant="standard"
+            value={nestedQuestion}
+            style={{ width: "465px", height: "147px" }}
+            onChange={(e) => setNestedQuestion(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleNestedClose}>Cancelar</Button>
+          <Button onClick={addNestedQuestion}>Adicionar</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
 
 const styles = {
-  form: {
-    marginTop: "20px",
+  container: {
+    maxWidth: "800px",
+    margin: "0 auto",
+    padding: "20px",
+    fontFamily: "Arial, sans-serif",
+  },
+
+  button: {
+    padding: "8px 10px",
+    fontSize: "14px",
+    backgroundColor: "#34a853",
+    color: "#FFF",
+    border: "none",
+    minWidth: "100px",
+    borderRadius: "4px",
+    cursor: "pointer",
+    marginRight: "10px",
+    "&:hover": {
+      backgroundColor: "#45a049",
+    },
+  },
+  buttonWithMargin: {
+    padding: "8px 10px",
+    fontSize: "16px",
+    backgroundColor: "#34a853",
+    color: "#FFF",
+    border: "none",
+    borderRadius: "4px",
+    minWidth: "100px",
+    cursor: "pointer",
+    marginRight: "20px",
+    "&:hover": {
+      backgroundColor: "#45a049",
+    },
   },
 };
 export default QuestionList;
